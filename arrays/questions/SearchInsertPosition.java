47c7b38852170e9bb7936db0286232e516c7d7d1
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-09-12 22:25:53 +0530

    Initial commit

--- arrays/questions/SearchInsertPosition.java
+++ arrays/questions/SearchInsertPosition.java
@@ -2,23 +2,22 @@ package arrays.questions;
 
 public class SearchInsertPosition {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 5, 4, 9};
+        int[] arr = { 1, 2, 5, 4, 9 };
         int target = 3;
-        int res =search(arr, target);
+        int res = search(arr, target);
         System.out.print(res);
     }
-    public static int search(int[] arr, int target){
+
+    public static int search(int[] arr, int target) {
         int n = arr.length;
-        int low=0, high= n-1;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(arr[mid] == target){
-               return mid;
-            }
-            else if(arr[mid] > target){
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
                 high = mid - 1;
-            }
-            else{
+            } else {
                 low = low + 1;
             }
         }
