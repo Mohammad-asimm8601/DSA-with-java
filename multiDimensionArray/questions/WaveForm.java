dda71d6467a206536db7e3a772531d269cc20176
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-09-19 19:46:50 +0530

    Initial commit

--- multiDimensionArray/questions/WaveForm.java
+++ multiDimensionArray/questions/WaveForm.java
@@ -1,37 +1,35 @@
 package multidimensionarray.questions;
 
-import java.util.Scanner;
-
 public class WaveForm {
 
-    public static void print(int[][] arr){
-        for(int[] ele : arr){
-            for(int x : ele){
-                System.out.print(x+" ");
+    public static void print(int[][] arr) {
+        for (int[] ele : arr) {
+            for (int x : ele) {
+                System.out.print(x + " ");
             }
             System.out.println();
         }
         System.out.println();
     }
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            int[][] arr = {{1, 2, 3}, {4, 5, 6},{7, 8, 9}};
-            print(arr);
-            int m = arr.length;
-            int n = arr[0].length;
 
-            for (int i = 0; i < m; i++) {
-                if(i%2 ==0 ){
-                    for (int j = 0; j < n; j++) {
-                        System.out.print(arr[i][j]+" ");
-                    }
+    public static void main(String[] args) {
+
+        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        print(arr);
+        int m = arr.length;
+        int n = arr[0].length;
+
+        for (int i = 0; i < m; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(arr[i][j] + " ");
                 }
-                else{
-                    for (int j = n-1; j >= 0 ; j--) {
-                        System.out.print(arr[i][j]+" ");
-                    }
+            } else {
+                for (int j = n - 1; j >= 0; j--) {
+                    System.out.print(arr[i][j] + " ");
                 }
             }
         }
+
     }
 }
