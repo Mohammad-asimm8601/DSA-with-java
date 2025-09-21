f600d96f9046e2e4b68a5b17d8fe6fe30c804da1
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-09-21 23:52:13 +0530

    Initial commit

---
+++ multiDimensionArray/questions/MultiplicationOfTwoMatrices.java
@@ -0,0 +1,26 @@
+// Write a program to print the multiplication of two matrices given by the user.
+package multidimensionarray.questions;
+
+public class MultiplicationOfTwoMatrices {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2, 1} ,{2, 1, 2}};
+        int[][] b = {{1, 0, 1, 2} ,{2, 1, 0, 0},{0, 3, 1, 1}};
+
+        if(a[0].length != b.length){
+            System.out.println("Multiplication not possible");
+        }
+        else{
+            int[][] c = new int[a.length][b[0].length];
+            for (int i = 0; i < c.length; i++) {
+                for (int j = 0; j < c[0].length; j++) {
+                    for (int k = 0; k < b.length; k++) {
+                        c[i][j] += (a[i][k] * b[k][j]);
+                    }
+                    System.out.print(c[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+ 
