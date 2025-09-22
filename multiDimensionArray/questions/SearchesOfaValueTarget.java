e25d0109b6269a47724777aac7940212b81c0539
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-09-23 01:56:27 +0530

    Initial main

--- multiDimensionArray/questions/SearchesOfaValueTarget.java
+++ multiDimensionArray/questions/SearchesOfaValueTarget.java
@@ -1,7 +1,7 @@
 /*  Write an efficient algorithm that searches for a value target in a an m x n integer matrix which has the following properties:
 
     -> Integers in each row are sorted in ascending from left to right.
-    -> Integers in each column are sorted in ascending from top to       bottom. 
+    -> Integers in each column are sorted in ascending from top to bottom. 
 
 */
 package multidimensionarray.questions;
@@ -9,7 +9,7 @@ package multidimensionarray.questions;
 import java.util.Scanner;
 
 public class SearchesOfaValueTarget {
-     public static void print(int[][] arr) {
+    public static void print(int[][] arr) {
         int m = arr.length;
         int n = arr[0].length;
         for (int i = 0; i < m; i++) {
@@ -20,30 +20,34 @@ public class SearchesOfaValueTarget {
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[][] arr = {{1, 4, 7, 11, 15},{2, 5, 8, 12, 19},{3, 6, 9, 16, 22},{10, 13, 14, 17, 24},{18, 21, 23, 26, 30}};
-        print(arr);
-        int target = 14;
-        boolean res = search(arr ,target);
-        System.out.println(res);
+        try (Scanner sc = new Scanner(System.in)) {
+            int[][] arr = { { 1, 4, 7, 11, 15 }, { 2, 5, 8, 12, 19 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 },
+                    { 18, 21, 23, 26, 30 } };
+            print(arr);
+            int target = sc.nextInt();
+            boolean res = search(arr, target);
+            System.out.println(res);
+        }
+
     }
 
-    public static boolean   search(int[][] arr, int target){
+    public static boolean search(int[][] arr, int target) {
         int m = arr.length;
         int n = arr[0].length;
 
-        int i =0, j=n-1;
-        while(i<m && j >=0){
-            if(arr[i][j] == target) return true;
-            else if(arr[i][j] > target){
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (arr[i][j] == target)
+                return true;
+            else if (arr[i][j] > target) {
                 j--;
-            }
-            else{
+            } else {
                 i++;
             }
         }
         return false;
-        
+
     }
 }
