d76b1c914a224a5d2c411cac2f807e3c329ed16a
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-09-29 00:36:24 +0530

    Initial commit

---
+++ string/StringArray.java
@@ -0,0 +1,10 @@
+package string;
+
+public class StringArray {
+    public static void main(String[] args) {
+        String[] arr = {"raghav", "asim", "karan", "shimla"};
+        for(String ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
