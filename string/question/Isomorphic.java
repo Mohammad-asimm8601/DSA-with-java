f0a08808ec6957068773de463830d42e393adf64
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-09-30 10:34:23 +0530

    Initial commit

--- string/question/Isomorphic.java
+++ string/question/Isomorphic.java
@@ -1,40 +1,44 @@
 // Given two string  s and t, determine if they are isomorphic.
 package string.question;
 
-import java.util.Scanner;
-
-public class Isomorphic {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        String t = sc.next();
-        int n = s.length();
-        char[] arr = new char[128];
-        for (int i = 0; i < n; i++) {
-            int ch = (int)s.charAt(i);
-            char dh = t.charAt(i);
-            if(arr[ch] == '\0') arr[ch] = dh;
-            else{
-                if(arr[ch] != dh) System.out.println(false);
-                else System.out.println(true);
-            }
+import java.util.*;
+public class Isomorphic
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		try (Scanner sc = new Scanner(System.in)) {
+            String s = sc.next();
+            String t = sc.next();
+            boolean b = isomorphic(s,t);
+            System.out.println(b);
         }
-        for (int i = 0; i < 128; i++) {
+	}
+    public static boolean isomorphic(String s, String t){
+        if(s.length() != t.length()) return false;
+        char[] arr = new char[128];
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            int ch =(int) s.charAt(i);
+            char dh = t.charAt(i);
+            if(arr[ch] =='\0') arr[ch] = dh;
+            else{
+                if(arr[ch] != dh) return false;
+            } 
+        }
+        for(int i=0; i<128; i++){
             arr[i] = '\0';
         }
-        for (int i = 0; i < n; i++) {
-            int ch = (int)t.charAt(i);
+        for(int i=0; i<n; i++){
+            int  ch =(int) t.charAt(i);
             char dh = s.charAt(i);
-            if(arr[ch] == '\0') arr[ch] = dh;
+            if(arr[ch] =='\0') arr[ch] = dh;
             else{
-                if(arr[ch] != dh) System.out.println(false);
-                else System.out.println(true);
-            }
+                if(arr[ch] != dh) return false;
+            } 
         }
-       
-        for (int i = 0; i < 128; i++) {
-            System.out.print(arr[i]+" ");
-        }
+        return true;
 
     }
 }
+
+
