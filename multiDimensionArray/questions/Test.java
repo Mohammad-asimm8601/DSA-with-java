11c296e88b2c5e23efc415d557e5e2a0efb08ff2
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-10-04 19:39:56 +0530

    Initial commit

--- multiDimensionArray/questions/Test.java
+++ multiDimensionArray/questions/Test.java
@@ -1,23 +1,14 @@
 package multidimensionarray.questions;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        int[] arr = {10, 8 ,3 ,4, 5, 6, 7};
-        int n = arr.length;
-        int mid = 3;
-        int left =mid - 1;
-        int right = mid + 1;
-        int leftmx =Integer.MIN_VALUE;
-        int rightmx = Integer.MIN_VALUE;
-        while (left >= 0) {
-            leftmx = Math.max(leftmx, arr[left]);
-            left--;
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            for (int i = 1; i < n; i+=i) {
+                System.out.println(i);
+            }
         }
-        while (right < n) {
-            rightmx = Math.max(rightmx, arr[right]);
-            right++;
-        }
-        System.out.println(leftmx);
-        System.out.println(rightmx);
     }
 }
--- string/Equals.java
+++ string/Equals.java
@@ -2,12 +2,12 @@ package string;
 
 public class Equals {
     public static void main(String[] args) {
-        String s = "abcxyz";
-        String a = "abcxyz";
-        String b = new String(s);
-        String c = "abc";
-        c = c + "xyz";
-        System.out.println(s == c);
-        System.out.println(s.equals(c));
+        // String s = "abcxyz";
+        // String a = "abcxyz";
+        // String b = new String(s);
+        // String c = "abc";
+        // c = c + "xyz";
+        // System.out.println(s == c);
+        // System.out.println(s.equals(c));
     }
 }
