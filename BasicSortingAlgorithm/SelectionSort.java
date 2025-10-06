52615f9de261bcf90cd735b9856f721278636b54
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-10-06 23:52:36 +0530

    Initial commit

---
+++ BasicSortingAlgorithm/SelectionSort.java
@@ -0,0 +1,24 @@
+package BasicSortingAlgorithm;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {-6, -4, 20, 1, -2, 8};
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int min = Integer.MAX_VALUE;
+            int mindx = -1;
+            for (int j = i; j < n; j++) {
+                if(arr[j] < min){
+                    min = arr[j];
+                    mindx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[mindx];
+            arr[mindx] = temp;
+        }
+        for (int ele : arr) {
+            System.out.print(ele+" ");
+        }
+    }
+}
