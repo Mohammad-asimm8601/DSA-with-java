dc72cc8c229408cd89a8917b06a7b4082328d9a4
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-10-16 22:49:19 +0530

    Initial commit

--- BinarySearch/Question/SmallestDivisor.java
+++ BinarySearch/Question/SmallestDivisor.java
@@ -2,37 +2,41 @@
 package BinarySearch.Question;
 
 public class SmallestDivisor {
-    public static boolean  isLess(int mid, int[] arr, int t) {
-        int sum =0;
+    public static boolean isLess(int mid, int[] arr, int t) {
+        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i]%mid == 0) sum += arr[i]/mid;
-            else sum += arr[i]/mid+1;
+            if (arr[i] % mid == 0)
+                sum += arr[i] / mid;
+            else
+                sum += arr[i] / mid + 1;
         }
         return sum <= t;
 
     }
+
     public static void main(String[] args) {
-        int[] arr = {44,22,33,11,1};
+        int[] arr = { 44, 22, 33, 11, 1 };
         int threshold = 5;
         int res = smallestDivisor(arr, threshold);
         System.out.println(res);
 
     }
-    public static int smallestDivisor(int[] arr, int t){
+
+    public static int smallestDivisor(int[] arr, int t) {
         int n = arr.length;
         int mx = Integer.MIN_VALUE;
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             mx = Math.max(mx, arr[i]);
         }
         int d = 1;
-        int lo= 1, hi = mx;
-        while(lo <= hi){
-            int mid = lo + (hi-lo)/2;
-            if(isLess(mid, arr, t) == true){
+        int lo = 1, hi = mx;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (isLess(mid, arr, t) == true) {
                 d = mid;
-                hi = mid-1;
-            }
-            else lo = mid +1;
+                hi = mid - 1;
+            } else
+                lo = mid + 1;
         }
         return d;
     }
