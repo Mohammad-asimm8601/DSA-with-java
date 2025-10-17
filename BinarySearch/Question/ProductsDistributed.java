702c7d5d1511698f8c093e6b2d9935e7625412a7
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-10-17 22:59:36 +0530

    Initial commit

---
+++ BinarySearch/Question/ProductsDistributed.java
@@ -0,0 +1,8 @@
+// Minimized maximun of products distributed to any store.
+package BinarySearch.Question;
+
+public class ProductsDistributed {
+    public static void main(String[] args) {
+        
+    }
+}
