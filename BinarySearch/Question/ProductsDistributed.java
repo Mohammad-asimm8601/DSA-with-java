cd670fa7af1871e0c6ebbc76a552f7e2a8973bd0
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-10-18 23:01:41 +0530

    Initial commit

--- BinarySearch/Question/ProductsDistributed.java
+++ BinarySearch/Question/ProductsDistributed.java
@@ -3,6 +3,6 @@ package BinarySearch.Question;
 
 public class ProductsDistributed {
     public static void main(String[] args) {
-        
+        System.out.println("Hello miss mahima darling");
     }
 }
