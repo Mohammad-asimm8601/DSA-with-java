112b0499cfc8d8057234886eb469bb84a86afd75
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-12-26 22:32:09 +0530

    Initial commit

---
+++ Stack/BasicStack.java
@@ -0,0 +1,23 @@
+package Stack;
+import java.util.Stack;
+public class BasicStack {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        System.out.println(st);
+        // System.out.println(st.peek());
+        System.out.println(st.pop());
+        System.out.println(st.size());
+        System.out.println(st);
+        // to access first element inserted in stack
+        while(st.size() > 1){
+            st.pop();
+        }
+        System.out.println(st);
+        System.out.println(st.isEmpty());
+    }
+}
