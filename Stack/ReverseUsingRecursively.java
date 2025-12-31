b75e83f1a3527818b84bf6b6e83de19b403fecba
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2025-12-31 16:30:41 +0530

    Initial commit

---
+++ Stack/ReverseUsingRecursively.java
@@ -0,0 +1,22 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ReverseUsingRecursively {
+    private static void stackRev(Stack<Integer> st){
+        if(st.isEmpty()) return;
+        int top = st.pop();
+        System.out.print(top+" ");
+        stackRev(st);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println(st);
+        stackRev(st);
+    }
+}
